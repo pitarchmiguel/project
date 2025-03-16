4be6861927fafd6ab33d716a7540d15c1f80586a
pitarchmiguel <pitarchmiguel@gmail.com>
2025-03-16 19:33:19 +0100

    Add more sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <header className="min-h-screen flex flex-col md:p-5 items-center justify-center gap-12">
+        <header className="min-h-screen flex flex-col md:p-5 items-center justify-center gap-12 p-4">
 
             <h1 className="md:text-6xl text-5xl font-black text-center">
                 Si no estás en internet, 
@@ -11,7 +11,7 @@ function Header() {
                 Todo lo que necesitas para conseguir más clientes e incrementar tus ventas
             </h2>
 
-            <button className="border-1 px-14 py-3 rounded-full bg-pink-700 text-pink-200 font-extrabold text-xl hover:bg-pink-900 hover:text-pink-100 ease-in-out duration-300 cursor-pointer">
+            <button className="border-1 border-pink-400 px-14 py-3 rounded-full bg-pink-700 text-pink-200 font-extrabold text-xl hover:bg-pink-900 hover:text-pink-100 ease-in-out duration-300 cursor-pointer hover:-rotate-10">
                 Hablamos
             </button>
 
